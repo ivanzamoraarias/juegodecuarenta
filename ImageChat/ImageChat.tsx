1cb7acd965fa18bb19053f26464fe62f18701aac
ivanzamoraarias <faucar93@gmail.com>
2020-08-11 23:53:34 +0200

    [JDC-1] update image chat component with effects

--- ImageChat/ImageChat.tsx
+++ ImageChat/ImageChat.tsx
@@ -2,18 +2,29 @@ import React, {useEffect, useState} from "react";
 import {Image, View, StyleSheet, Text, TextInput, TouchableHighlight, ScrollView} from "react-native";
 import {WebView} from "react-native-webview";
 import {FirebaseService} from "../Firebase/Firebase";
+import MemoryService from "../Storage/Memory/MemoryService";
+
 
 const imageUri: string = "https://i.pinimg.com/originals/ed/83/7a/ed837acb91e3ee1a42c46538b509b504.jpg";
 //https://1.bp.blogspot.com/-slPQMdRIiE0/XchjiHQcOgI/AAAAAAAALm8/OK-GsqynYm4qhL7DuCvnyzN24etS76jOACKgBGAsYHg/s1600/IMG_20191028_130922.jpg
 
+let algo:string = "";
 const ImageChat = () => {
+    //MemoryService.initializeMemoryService();
+    useEffect(()=>{
+        MemoryService.addMemoryKey("messages");
+    },[]);
     const [ownMessage, setOwnMessage] = useState("");
     const [messages, setMessages] = useState([""]);
     const [partnerName, setPartnerName] = useState("");
     const [ownName, setOwnName] = useState("");
+    const [partnerMessage, setPartnerMessage] = useState("");
+    const [mostrat, setMostrar] = useState("");
+    const [val, setVal] = useState(0);
 
     useEffect(() => {
-        FirebaseService.initializeInstance();
+        FirebaseService
+            .initializeInstance();
         FirebaseService
             .disconnectFirebase();
         FirebaseService
@@ -22,15 +33,24 @@ const ImageChat = () => {
                 (value: { lastOne: string }) => {
                     if(!value)
                         return;
-                    // @ts-ignore
-                    let array: string [] = [...messages];
-                    array.push(`${value.lastOne}`);
+
+                    console.log("Updated from firebase", value.lastOne);
+                    setPartnerMessage(value.lastOne);
+                    setMostrar(`\n${mostrat}\n${value.lastOne}`);
+                    algo += `\n${value.lastOne}`;
+
+                    MemoryService.pushElementToKey("messages",value.lastOne);
+
+                    setMessages(MemoryService.getElementByKey("messages"));
 
 
-                    setMessages(array);
                 });
 
     }, [partnerName])
+
+    useEffect(() => {
+        setVal(1);
+    }),[partnerMessage];
 
 
     const appendMessage = (message: string) => {
@@ -39,17 +59,32 @@ const ImageChat = () => {
     const addMessages = () => {
         const newMessage:string = `${ownMessage}`;
         // @ts-ignore
-        setMessages([...messages, newMessage]);
+        //setMessages([...messages, newMessage]);
         FirebaseService.storeNewOwnMessage(ownName, ownMessage);
+        MemoryService.pushElementToKey("messages",newMessage);
+        setMessages(MemoryService.getElementByKey("messages"));
+
+        setMostrar(`\n${mostrat}\n${newMessage}`);
+        algo += `\n${newMessage}`;
+
     }
     const getMessagesStringAsStack = () => {
+       // const memoryMessages = MemoryService.getElementByKey("messages");
         let result: string = "";
+        /*if(memoryMessages === [] || memoryMessages === undefined )
+            return result;*/
 
+
+        /*memoryMessages.forEach((m: string) => {
+            result = `${result}\n${m}`;
+        });*/
         messages.forEach((m: string) => {
             result = `${result}\n${m}`;
         });
         return result;
     }
+
+
     return (
         <View style={styles.container}>
             <TextInput
@@ -70,9 +105,10 @@ const ImageChat = () => {
             <View style={styles.messagesContainer}>
                 <ScrollView>
                     <Text style={styles.text}>
-                        {getMessagesStringAsStack()}
+                        {messages.map((m:string)=>`\n${m}`)}
                     </Text>
                 </ScrollView>
+                <Text>{partnerMessage}</Text>
             </View>
 
             <TextInput
---
+++ Storage/Memory/MemoryService.ts
@@ -0,0 +1,59 @@
+class MemoryService {
+    private static memory: { store: { [k: string]: any[]; } }= {store:{}};
+
+    public static initializeMemoryService(){
+        // console.log("Mierdaaa", MemoryService.memory);
+        // console.log("Tipo de la m", typeof MemoryService.memory);
+        //
+        // if(MemoryService.memory !== null ||
+        //     MemoryService.memory !== undefined) {
+        //     console.log("Viene aca ?");
+        //     return;
+        // }
+        // console.log("Mierda dos ?");
+        // MemoryService.memory = {
+        //     store:{}
+        // };
+        //console.log("STOREEEE", MemoryService.memory);
+    }
+
+    public static addMemoryKey(key: string) {
+        if(MemoryService.memory.store[`${key}`] !== undefined)
+            return;
+        if (MemoryService.memory.store === undefined) {
+            return;
+        }
+
+        MemoryService.memory.store = {
+            ...MemoryService.memory.store,
+            [`${key}`]: []
+        };
+    }
+
+    public static pushElementToKey(key: string, value: string): boolean {
+        if (!MemoryService.memory.store[`${key}`]) {
+            return false;
+        }
+
+        MemoryService.memory.store[`${key}`].push(value);
+        return true;
+    }
+
+    public static getLastElementByKey(key: string) {
+        const storeArray: any[] = MemoryService.memory.store[`${key}`];
+        return storeArray[storeArray.length-1];
+    }
+
+    public static getElementByKey(key: string):any[]{
+        if(MemoryService.memory.store === undefined)
+            return [];
+        const storeArray: any[] =
+            MemoryService.memory.store[`${key}`] === undefined?[]:
+                MemoryService.memory.store[`${key}`]
+        ;
+        return storeArray;
+    }
+
+}
+
+export default MemoryService;
